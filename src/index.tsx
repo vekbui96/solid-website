b16d64f358715cbecec645435eed2203f546b10e
vekbui96 <tommy.bui1428@gmail.com>
2024-02-05 14:17:16 -0600

    feat: added gh

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import { Header } from './pages/Header/header.jsx'
 import { HeroArea } from './pages/HeroArea/HeroArea.jsx'
 import { SocialMedia } from './pages/SocialMedia/SocialMedia.jsx'
 import { About } from './pages/About/About.jsx'
+import { Testing } from '../src/components/Testing.jsx'
 
 const MainPage = () => {
     return (<>
         <Header/>
         <HeroArea/>
         <About/>
+        <Testing/>
     </>)
 }
 
